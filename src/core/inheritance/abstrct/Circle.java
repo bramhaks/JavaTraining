2b13301a192ed90daae97540e806d04e9196d430
bramhaks <32040397+bramhaks@users.noreply.github.com>
2017-12-09 10:31:13 -0500

    polymorphism

--- src/core/inheritance/abstrct/Circle.java
+++ src/core/inheritance/abstrct/Circle.java
@@ -3,11 +3,14 @@ package core.inheritance.abstrct;
 public class Circle extends GraphicObject {
 
 	@Override
+	public void moveTo(int newX, int newY) {
+		System.out.println("moveTo method in circle has been overriden with different implementation.");
+	}
+	
 	public void draw() {
 		System.out.println("This is draw method inside circle.");
 	}
 
-	@Override
 	public void resize() {
 		System.out.println("This is resize method inside circle.");
 	}
